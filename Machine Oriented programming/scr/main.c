31f657b5abb0bfe7be482a9662341f9bbad39334
DanielFS <112876011+DanielFS211@users.noreply.github.com>
2025-05-07 17:28:32 +0200

    Small text added

--- Machine Oriented programming/scr/main.c
+++ Machine Oriented programming/scr/main.c
@@ -103,7 +103,7 @@ int main() {
 
         layoutPrint(columns);
         //print_deck(deck);
-        printf("\nEnter SW to Show Cards, HC to Hide Cards, SR to Shuffle Cards, SD <filename> to Save Deck, or QQ to EXIT.\n");
+        printf("\nEnter SW to Show Cards, HC to Hide Cards, SR to Shuffle Cards, SI to Split and Shuffle Cards, SD <filename> to Save Deck, or QQ to EXIT.\n");
         printf("Enter Input:  ");
         scanf("%3s", input);
 
